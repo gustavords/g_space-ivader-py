47146300a00b2b801aeb2fd170b2cf0ca7da8f09
gustavords <22622146+gustavords@users.noreply.github.com>
2025-10-16 00:03:07 -0400

    Basic Game Setup

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+print(pygame.ver);
+
+
+
+# general setup
+pygame.init();
+ 
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+
+running = True
+
+while running:
+  # event loop
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+       running = False
+       
+  # draw game
+  
+pygame.QUIT()
