12b2d72fef7e812bdb2399c5a1246d775546d0b8
gustavords <22622146+gustavords@users.noreply.github.com>
2025-10-16 14:54:21 -0400

    Fix linting issue

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+# pylint: disable=no-member
 import pygame
-print(pygame.ver);
-
 
 
 # general setup
-pygame.init();
- 
+pygame.init()
+
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
-display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("Pretty Please bruh")
 
 running = True
 
+# surface
+surf = pygame.Surface((100, 200))
+surf.fill("orange")
+
 while running:
-  # event loop
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-       running = False
-       
-  # draw game
-  
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw game
+    display_surface.fill("darkgrey")
+    display_surface.blit(surf, (100, 150))
+    pygame.display.update()
+
+
 pygame.QUIT()
